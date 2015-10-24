a9ecd4af9584bff037f37b31c8a687ae2beb6cf2
Audrey Scrugham <audrey@epicodus.com>
2015-10-23 23:34:48 -0700

    passing spec for 1 bottle

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@
 // var subtract = userInput - 1;
 
 var bottlesOfBeer = function(number) {
-  return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
+  if (number == 0) {
+    return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
+  } else {
+    return "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.";
+  }
 }
-// };
--- specs/specs.js
+++ specs/specs.js
@@ -2,7 +2,7 @@ describe('bottlesOfBeer', function() {
   it("returns the lyrics for 0 bottles of beer", function() {
     expect(bottlesOfBeer(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
-  // it("adds user input - 1 to repeated main verse of song", function() {
-  //   expect(bottlesOfBeer(userInput - 1)).to.equal(subtract + "" + mainVerse);
-  // });
+  it("returns the lyrics for 1 bottle of beer", function() {
+    expect(bottlesOfBeer(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.");
+  });
 });
