c7b1489c711f27b3f296e7065a9a564073ab3540
Audrey Scrugham <audrey@epicodus.com>
2015-10-23 23:20:15 -0700

    add bdd text file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-var verse1 = "bottles of beer on the wall,";
+var mainVerse = "bottles of beer on the wall,";
 var verse2 = "bottles of beer. Take one down and pass it around,";
 var verse3 = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more,";
 
 var userInput = prompt("Enter a number");
+var subtract = userInput - 1;
 
 var bottlesOfBeer = function(number) {
   if (userInput > 0) {
-    return (userInput + verse1)
+    return (userInput + "" + mainVerse + "" + userInput + "" + verse2 + "" + subtract + "" + mainVerse)
   } else {
     return verse3
   }
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,8 @@
 describe('bottlesOfBeer', function() {
-  it("adds user input to the bottles of beer song", function() {
-    expect(bottlesOfBeer(userInput)).to.equal(userInput + "" + verse1);
+  it("adds user input to the main verse and second verse of the bottles of beer song", function() {
+    expect(bottlesOfBeer(20)).to.equal(userInput + "" + mainVerse + "" + userInput + "" + verse2);
+  });
+  it("adds user input - 1 to repeated main verse of song", function() {
+    expect(bottlesOfBeer(userInput - 1)).to.equal(subtract + "" + mainVerse);
   });
 });
